38e0a70f884e599505b308881d3897373ffe3daa
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-11-24 23:43:13 +0500

    object

---
+++ lesson 6/app.js
@@ -0,0 +1,67 @@
+// Exercise 1
+
+// const user = {};
+// user.name = "Dostonbek";
+// user.surname = "Abdusattarov";
+// user.name = "Bob";
+// delete user.name;
+
+// console.log(user);
+
+// Exercise 2 
+
+// let isEmpty = (obj) => {
+//   for (let i in obj) {
+//     return false;
+//   }
+//   return true;
+// };
+
+// let schedule = {};
+
+// console.log(isEmpty(schedule));
+
+// schedule["5:00"] = "get up";
+
+// console.log(isEmpty(schedule));
+
+// Exercise 3
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+
+// let sum = null;
+
+// for (let i in salaries) {
+//   sum += salaries[i];
+// }
+
+// console.log(sum);
+
+// Exercise 4
+
+// let multiply = (obj) => {
+//   for (let i in obj) {
+//     if (typeof obj[i] == "number") {
+//       obj[i] *= 2;
+//     }
+//   }
+// };
+
+// let box = {
+//   width: 100,
+//   height: 200,
+//   title: "box",
+// };
+
+// multiply(box);
+
+// console.log(box);
+
+
+
+
+
